ffc25bd149f0bc349fac3b25dc64fc82df2143f8
James Nixon <jamesnixon197@gmail.com>
2020-12-23 18:17:02 +0000

    Set up jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    clearMocks: true,
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    setupFiles: ['./test/helpers.ts'],
+    reporters: [
+        'default',
+        [
+            'jest-slow-test-reporter',
+            {
+                numTests: 3,
+                warnOnSlowerThan: 300,
+                color: true,
+            },
+        ],
+    ],
+    transform: {'\\.ts$': ['ts-jest']},
+};
